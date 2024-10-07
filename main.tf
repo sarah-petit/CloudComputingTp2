b20027df2e44593ec6c8173ee11731c102b1b494
sarah-petit <60597342+sarah-petit@users.noreply.github.com>
2024-10-07 15:00:26 +0200

    version qui fonctionne

--- main.tf
+++ main.tf
@@ -61,6 +61,48 @@ resource "azurerm_storage_container" "storage_container" {
   container_access_type = "private"
 }
 
+resource "azurerm_storage_blob" "blob" {
+  name                   = "appTP2.zip"
+  storage_account_name   = azurerm_storage_account.storage.name
+  storage_container_name = azurerm_storage_container.storage_container.name
+  type                   = "Block"
+  source                 = "C:/Users/sarah/OneDrive/Documents/études/M2/Cloud computing/TP/Provisionnement d'une base de donnée NoSql et d'un blob de stockage/CloudComputingTp2/API Python/test/appTP2.zip"
+}
+
+
+data "azurerm_storage_account_sas" "sas" {
+  connection_string = azurerm_storage_account.storage.primary_connection_string
+
+  start = "2024-10-02T00:00:00Z"
+  expiry = "2025-04-30T00:00:00Z" # Date d'expiration de la SAS
+  
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  resource_types {
+    service   = false
+    container = false
+    object    = true
+  }
+
+  permissions {
+    read    = true
+    write   = false
+    delete  = false
+    list    = false
+    add     = false
+    create  = false
+    update  = false
+    process = false
+    tag     = false
+    filter  = false
+  }
+}
+
 #Create app service
 resource "azurerm_service_plan" "plan" {
   name                = "plan-${random_string.random.result}"
@@ -75,12 +117,15 @@ resource "azurerm_linux_web_app" "app" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_service_plan.plan.location
   service_plan_id     = azurerm_service_plan.plan.id
-  zip_deploy_file = "C:/Users/sarah/OneDrive/Documents/études/M2/Cloud computing/TP/Provisionnement d'une base de donnée NoSql et d'un blob de stockage/CloudComputingTp2/API Python/test/appTP2.zip"
-
+  
   site_config {
+    application_stack {
+      python_version = 3.12
+    }
   }
   
   app_settings = {
-    WEBSITE_RUN_FROM_PACKAGE = 1
+    WEBSITE_RUN_FROM_PACKAGE = "https://${azurerm_storage_account.storage.name}.blob.core.windows.net/${azurerm_storage_container.storage_container.name}/${azurerm_storage_blob.blob.name}?${data.azurerm_storage_account_sas.sas.sas}"
+    SCM_DO_BUILD_DURING_DEPLOYMENT = true
   }
 }
